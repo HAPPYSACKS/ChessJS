2ea52649ed52ddc00e4f6e65e9629cf79d80116b
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2023-10-15 22:43:43 -0600

    added avaatar + changed background

--- Bishop.js
+++ Bishop.js
@@ -1,8 +1,8 @@
 import chessPiece from "./ChessPiece.js";
 
 export default class Bishop extends chessPiece {
-  whiteImage = "./img/black_bishop.svg";
-  blackImage = "./img/white_bishop.svg";
+  whiteImage = "./img/white_bishop.svg";
+  blackImage = "./img/black_bishop.svg";
   constructor(color, position) {
     super(color, position);
     this.setDisplayedImage();
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import Chessboard from "./Chessboard.js";
-
+    
 let chessboard = new Chessboard();
 chessboard.initializeBoard();
 chessboard.renderBoard();
