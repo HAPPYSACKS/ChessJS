d440b6c8c7d298ff7675a24323a28b4b0162bc66
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2023-11-02 02:33:20 -0600

    Added isValidMove for pawn

--- Pawn.js
+++ Pawn.js
@@ -5,10 +5,42 @@ export default class Pawn extends chessPiece {
   whiteImage = "./img/white_pawn.svg";
   blackImage = "./img/black_pawn.svg";
   constructor(color, position) {
-    super(color, position); 
-    this.setDisplayedImage(); 
+    super(color, position);
+    this.setDisplayedImage();
   }
-  canPromote() { 
+  canPromote() {
     // maybe implement, depending on if needed, or using listeners to implement this.
+  }
+
+  isValidMove(targetPosition) {
+    let possibleMoves = [];
+
+    if ((this, color === "white" && this.firstMove)) {
+      possibleMoves.push({
+        row: targetPosition.row - 2,
+        col: targetPosition.col,
+      });
+    }
+
+    if (this.color === "white") {
+      possibleMoves.push({
+        row: targetPosition.row - 1,
+        col: targetPosition.col,
+      });
+    }
+
+    if ((this, color === "black" && this.firstMove)) {
+      possibleMoves.push({
+        row: targetPosition.row + 2,
+        col: targetPosition.col,
+      });
+    }
+
+    if (this.color === "white") {
+      possibleMoves.push({
+        row: targetPosition.row + 1,
+        col: targetPosition.col,
+      });
+    }
   }
 }
